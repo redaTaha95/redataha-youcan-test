ba6a309b71aae94f7158af308bf6f073994d259f
redaTaha95 <redataha27@gmail.com>
2022-07-15 19:13:55 +0100

    creation of Category Model with his migration and factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -23,5 +24,6 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         Product::factory(10)->create();
+        Category::factory(1)->create();
     }
 }
---
+++ tests/Feature/ProductTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Product;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ProductTest extends TestCase
+{
+
+    /** @test */
+    public function users_can_create_a_new_product () {
+
+
+    }
+}
