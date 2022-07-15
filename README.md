0908bfcfa9fe8bc10ecd7dbe24e23c0e19946e55
redaTaha95 <redataha27@gmail.com>
2022-07-15 16:37:17 +0100

    Initializing The Repository Design Pattern

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 Reda TAHA Youcan Test
 ------------------------------------
 Coding Challenge Software Engineer application by Reda TAHA
+
+<h5>- About Design Pattern</h5>
+Project developed with the repository design pattern
+=> All basics methods (CRUD) included in the BaseRepository <br>
+--app <br>
+----repositories <br>
+------Interfaces <br>
+--------BaseRepositoryInterface.php <br>
+------BaseRepository.php <br>
+<br>
+<strong>Repository Service Provider has been generated with CLI:</strong>
+<pre><code>
+php artisan make:provider RepositoryServiceProvider
+</code></pre>
---
+++ app/repositories/BaseRepository.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace App\repositories;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class BaseRepository implements interfaces\BaseRepositoryInterface
+{
+
+    private $model;
+
+    public function __construct(Model $model)
+    {
+
+        $this->model = $model;
+    }
+
+    public function all()
+    {
+        return $this->model->latest()->get();
+    }
+
+    public function create(array $data)
+    {
+        return $this->model->create($data);
+    }
+
+    public function update(array $data, $id)
+    {
+        $record = $this->find($id);
+        return $record->update($data);
+    }
+
+    public function delete($id)
+    {
+        return $this->model->destroy($id);
+    }
+
+    public function find($id)
+    {
+        return $this->model->findOrFail($id);
+    }
+}
